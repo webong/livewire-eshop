40687f6ebac9606c6e961c707bc9fd903b372949
wisdomanthoni <wizitendo10@gmail.com>
2019-08-04 20:54:36 +0100

    laravel eshop setup ready

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,97 @@
+<div class="shopping-cart">
+  <!-- Head -->
+  <div class="header">
+    Shopping Cart
+    <button class="checkout">Checkout</button>
+  </div>
+
+  <!-- Product #1 -->
+  <div class="cart-item">
+    <div class="buttons">
+      <span class="delete-btn"></span>
+      <span class="like-btn"></span>
+    </div>
+
+    <div class="image">
+      <img src="shoe1.png" alt="" />
+    </div>
+
+    <div class="description">
+      <span>Common Projects</span>
+      <span>Bball High</span>
+      <span>White</span>
+    </div>
+
+    <div class="quantity">
+      <button class="plus-btn" type="button" name="button">
+        <img src="plus.svg" alt="" />
+      </button>
+      <input type="text" name="name" value="1">
+      <button class="minus-btn" type="button" name="button">
+        <img src="minus.svg" alt="" />
+      </button>
+    </div>
+
+    <div class="total-price">$549</div>
+  </div>
+
+  <!-- Product #2 -->
+  <div class="cart-item">
+    <div class="buttons">
+      <span class="delete-btn"></span>
+      <span class="like-btn"></span>
+    </div>
+
+    <div class="image">
+      <img src="/jacket1.png" alt=""/>
+    </div>
+
+    <div class="description">
+      <span>Maison Margiela</span>
+      <span>Future Sneakers</span>
+      <span>White</span>
+    </div>
+
+    <div class="quantity">
+      <button class="plus-btn" type="button" name="button">
+        <img src="plus.svg" alt="" />
+      </button>
+      <input type="text" name="name" value="1">
+      <button class="minus-btn" type="button" name="button">
+        <img src="minus.svg" alt="" />
+      </button>
+    </div>
+
+    <div class="total-price">$870</div>
+  </div>
+
+  <!-- Product #3 -->
+  <div class="cart-item">
+    <div class="buttons">
+      <span class="delete-btn"></span>
+      <span class="like-btn"></span>
+    </div>
+
+    <div class="image">
+      <img src="shirt1.png" alt="" />
+    </div>
+
+    <div class="description">
+      <span>Our Legacy</span>
+      <span>Brushed Scarf</span>
+      <span>Brown</span>
+    </div>
+
+    <div class="quantity">
+      <button class="plus-btn" type="button" name="button">
+        <img src="plus.svg" alt="" />
+      </button>
+      <input type="text" name="name" value="1">
+      <button class="minus-btn" type="button" name="button">
+        <img src="minus.svg" alt="" />
+      </button>
+    </div>
+
+    <div class="total-price">$349</div>
+  </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::view('/', 'store');
