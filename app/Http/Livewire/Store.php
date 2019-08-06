c5aebb0481058e85dc3ea59b3c9c66fa12f7ba00
wisdomanthoni <wizitendo10@gmail.com>
2019-08-07 00:40:50 +0100

    fixed store component

--- app/Http/Livewire/Store.php
+++ app/Http/Livewire/Store.php
@@ -9,25 +9,27 @@ class Store extends Component
 {
     public $items;
 
+    public $category;
+
     protected $listeners = [
         'filterItems' => 'filterItems',
-    ];
-    protected $category = [
-        'women', 'men', 'sale'
     ];
 
     public function mount($category = null)
     {
         switch ($category) {
             case null:
+                $this->category = $category;
                 $this->items = Item::all();
                 break;
             case 'men':
             case 'women':
-                $this->items = Item::where('category', $category);
+                $this->category = $category;
+                $this->items = Item::where('category', $category)->get();
                 break;
             case 'sale':
-                $this->items =Item::where('sale', true)->get();
+                $this->items = Item::where('sale', true)->get();
+                break;
             default:
                 return abort(404);
                 break;
@@ -36,15 +38,24 @@ class Store extends Component
 
     public function filterItems(float $min, float $pricerange, bool $sale)
     {
+        $this->sale = $sale;
+        $category = $this->category;
+
         if($sale) {
             $this->items = Item::where('sale', $sale)
+                ->when(isset($category), function($query) use ($category) {
+                    $query->where('category', $category);
+                })
                 ->when(isset($pricerange), function($query) use ($min,$pricerange) {
                     $query->whereBetween('price', [$min, $pricerange]);
                 })
                 ->get();
         } else {
             $this->items = Item::whereBetween('price', [$min, $pricerange])
-            ->get();
+                ->when(isset($category), function($query) use ($category) {
+                    $query->where('category', $category);
+                })
+                ->get();
         }
     }
 
