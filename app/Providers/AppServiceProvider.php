8639985dac6a61f24bbefaaf078e155773b8eb97
wisdomanthoni <wizitendo10@gmail.com>
2019-08-07 03:34:21 +0100

    update service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +17,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         Schema::defaultStringLength(191);
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
 
     }
 
